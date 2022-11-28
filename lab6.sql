c24310c79e730f3dad379367cc2fdfe782d51f2f
aleksl11 <110901701+aleksl11@users.noreply.github.com>
2022-11-28 12:30:23 +0100

    Update lab6.sql

--- lab6.sql
+++ lab6.sql
@@ -14,3 +14,19 @@ update Pracownicy set pensja = 5000 where IDENTYFIKATOR=1
 delete from Pracownicy where IDENTYFIKATOR=17
 
 --4.Zdefiniuj wyzwalacz, który zabroni zmniejszania pensji pracownikom
+
+create trigger zmiana 
+on Pracownicy
+after update
+as
+if update(pensja)
+begin
+	update Pracownicy
+	set pensja= deleted.pensja
+	from Pracownicy inner join deleted on Pracownicy.IDENTYFIKATOR=deleted.IDENTYFIKATOR
+	where Pracownicy.pensja<deleted.pensja and Pracownicy.IDENTYFIKATOR=deleted.identyfikator
+end
+
+update Pracownicy set pensja =5000 where IDENTYFIKATOR=1
+
+
