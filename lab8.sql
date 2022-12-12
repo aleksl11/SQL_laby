d4bf1014cbd483fc015f656ad04abfc54ea8bbad
aleksl11 <110901701+aleksl11@users.noreply.github.com>
2022-12-12 12:28:43 +0100

    Create lab8.sql

---
+++ lab8.sql
@@ -0,0 +1,24 @@
+--	Stwórz tabele Pracownik={Imie, Nazwisko, Pesel, Plec}. Zaprogramuj wyzwalacz, który dla nowowprowadzonych pracowników będzie uzupełniał kolumnę Plec na podstawie numeru pesel.
+create table Pracownik(
+	id_pracownik int identity(1,1) primary key,
+	Imie varchar(25),
+	Nazwisko varchar(25),
+	Pesel varchar(11),
+	Płeć varchar(1)
+)
+
+alter trigger plec on Pracownik
+after insert
+as
+begin
+	 update Pracownik set Płeć=
+	case
+	when ((select substring(inserted.Pesel,10,1) from inserted)in ('0','2','4','6','8'))
+	then 'K'
+	else 'M'
+	end
+	from inserted inner join Pracownik on inserted.id_pracownik=Pracownik.id_pracownik
+end
+
+insert into Pracownik(imie,nazwisko,pesel) values ('fr','fdhga','12344567865')
+select * from Pracownik
